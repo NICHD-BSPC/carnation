2034538900e1175dd7f1f12254f3f6ed63c7afe3
Apratim Mitra <apratim.mitra@nih.gov>
2025-12-20 15:24:55 -0500

    add dendrogram ex

--- R/enrich-plots.R
+++ R/enrich-plots.R
@@ -1468,11 +1468,97 @@ alluvialServer <- function(id, obj, res_obj, config){
 
 #################### Dendrogram ################################################
 
-#' Dendrogram plot UI
+#' Dendrogram module
 #'
-#' @param id ID string used to match the ID used to call the module server function
+#' @description
+#' UI & module to generate dendrograms.
+#'
+#' @param id Module id
 #' @param panel string, can be 'sidebar' or 'main'
+#' @param obj reactiveValues object containing GeneTonic object
+#' @param config reactive list with config settings
 #'
+#' @returns
+#' UI returns tagList with plot UI
+#' server invisibly returns NULL (used for side effects)
+#'
+#' @examples
+#' library(airway)
+#' library(DESeq2)
+#' library(org.Hs.eg.db)
+#'
+#' # load airway data
+#' data('airway')
+#'
+#' # extract counts and metadata
+#' mat <- assay(airway)
+#' cdata <- colData(airway)
+#'
+#' # get symbol annotations
+#' anno_df <- mapIds(org.Hs.eg.db,
+#'                column='SYMBOL',
+#'                keys=rownames(mat),
+#'                keytype='ENSEMBL')
+#'
+#' # analyze with DESeq2
+#' dds <- DESeqDataSetFromMatrix(mat,
+#'                               colData=cdata,
+#'                               design=~cell + dex)
+#' dds <- DESeq(dds)
+#'
+#' # extract comparison of interest
+#' res <- results(dds, contrast = c("dex", "trt", "untrt"))
+#'
+#' # add gene column from rownames
+#' res$gene <- rownames(res)
+#'
+#' # add symbol column from annotations
+#' res$symbol <- anno_df[rownames(res)]
+#'
+#'
+#' # get DE genes with FDR < 0.1
+#' de_genes <- rownames(res)[res$padj < 0.1 & !is.na(res$padj)]
+#'
+#' \dontrun{
+#' # functional enrichment using GO BP
+#' eres <- clusterProfiler::enrichGO(
+#'             gene = de_genes[1:100],
+#'             keyType = 'ENSEMBL',
+#'             OrgDb=org.Hs.eg.db,
+#'             ont='BP'
+#'         )
+#'
+#' # convert to GeneTonic object
+#' gt <- GeneTonic::shake_enrichResult(eres)
+#'
+#' obj <- reactive({
+#'   list(l_gs = gt$l_gs,
+#'        anno_df = gt$anno_df,
+#'        label = 'comp1')
+#' })
+#'
+#' config <- reactiveVal(get_config())
+#'
+#' # run simple shiny app with plot
+#' if(interactive()){
+#'   shinyApp(
+#'     ui = fluidPage(
+#'            sidebarPanel(dendrogramUI('p', 'sidebar')),
+#'            mainPanel(dendrogramUI('p', 'main'))
+#'          ),
+#'     server = function(input, output, session){
+#'                dendrogramServer('p', obj, config)
+#'              }
+#'   )
+#' }
+#'
+#' }
+#
+#' @rdname dendromod
+#' @name enrichplotmod
+NULL
+
+#' @rdname dendromod
 #' @export
 dendrogramUI <- function(id, panel){
   ns <- NS(id)
@@ -1534,12 +1620,7 @@ dendrogramUI <- function(id, panel){
   }
 }
 
-#' Dendrogram plot server function
-#'
-#' @param id ID string used to match the ID used to call the module UI function
-#' @param obj reactive containing GeneTonic object
-#' @param config reactive list with config settings
-#'
+#' @rdname dendromod
 #' @export
 dendrogramServer <- function(id, obj, config){
 
